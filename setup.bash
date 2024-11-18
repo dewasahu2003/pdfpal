61922c80bb474eebb9164e806534f3f52ebab6f9
Dewa <95997298+dewasahu2003@users.noreply.github.com>
2024-11-18 07:57:06 +0000

    event:added changes for win and linux

---
+++ setup.bash
@@ -0,0 +1,5 @@
+python -m venv venv
+source venv/bin/activate
+pip install -r reqtiny.txt
+
+docker compose up
---
+++ tiny-windows.bat
@@ -0,0 +1,1 @@
+wdget -L https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf -o tinyllama-1.1b-chat-v1.0.gguf
